ec88f4769425dd56da49cd9bb044da275cf31903
Nick <maniutin@gmail.com>
2023-12-11 14:54:54 -0500

    add styling and minor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,25 @@ function App() {
     <div className="App">
       <Router>
         <Nav />
-        <Routes>
-          <Route path="/" element={<PostList path={"/"} />} />
-          <Route path="/reviews" element={<PostList path={"/reviews"} />} />
-          <Route path="/reviews/:id" element={<Post />} />
-          <Route
-            path="/interviews"
-            element={<PostList path={"/interviews"} />}
-          />
-          <Route path="/interviews/:id" element={<Post />} />
-          <Route path="/listen" element={<PostList path={"/listen"} />} />
-          <Route path="/listen/:id" element={<Post />} />
-          <Route
-            path="/3-word-reviews"
-            element={<PostList path={"/3-word-reviews"} />}
-          />
-          <Route path="/about" element={<PostList path={"/about"} />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<PostList path={"/"} />} />
+            <Route path="/reviews" element={<PostList path={"/reviews"} />} />
+            <Route path="/reviews/:id" element={<Post />} />
+            <Route
+              path="/interviews"
+              element={<PostList path={"/interviews"} />}
+            />
+            <Route path="/interviews/:id" element={<Post />} />
+            <Route path="/listen" element={<PostList path={"/listen"} />} />
+            <Route path="/listen/:id" element={<Post />} />
+            <Route
+              path="/3-word-reviews"
+              element={<PostList path={"/3-word-reviews"} />}
+            />
+            <Route path="/about" element={<PostList path={"/about"} />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,144 +8,134 @@ import ScLogo from "../../assets/social_logos/sc_logo.png";
 
 const Nav = () => {
   return (
-    <div>
-      <nav className="navbar">
-        <div>
-          <NavLink to="/">
-            <img src={Logo} alt="oddboll logo" className="nav-logo" />
+    <nav className="navbar">
+      <div>
+        <NavLink to="/">
+          <img src={Logo} alt="oddboll logo" className="nav-logo" />
+        </NavLink>
+      </div>
+      <div className="nav-list">
+        <div className="nav-link">
+          <NavLink
+            to="/"
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+          >
+            Home
           </NavLink>
         </div>
-        <div className="nav-list">
-          <div className="nav-link">
-            <NavLink
-              to="/"
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-            >
-              Home
-            </NavLink>
-          </div>
 
-          <div className="nav-link">
-            <NavLink
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-              to="/reviews"
-            >
-              Reviews
-            </NavLink>
-          </div>
-
-          <div className="nav-link">
-            <NavLink
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-              to="/interviews"
-            >
-              Interviews
-            </NavLink>
-          </div>
-
-          <div className="nav-link">
-            <NavLink
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-              to="/listen"
-            >
-              Listen
-            </NavLink>
-          </div>
-
-          <div className="nav-link">
-            <NavLink
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-              to="/3-word-reviews"
-            >
-              3-Word Reviews
-            </NavLink>
-          </div>
-
-          <div className="nav-link">
-            <NavLink
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      textDecoration: "underline",
-                    }
-                  : {}
-              }
-              to="/about"
-            >
-              About
-            </NavLink>
-          </div>
+        <div className="nav-link">
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+            to="/reviews"
+          >
+            Reviews
+          </NavLink>
         </div>
-        <div className="nav-social-logos">
-          <a
-            href="https://www.instagram.com/odddboll/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nav-social-link"
+
+        <div className="nav-link">
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+            to="/interviews"
           >
-            <img
-              src={IgLogo}
-              alt="instagram logo"
-              className="nav-social-icon"
-            />
-          </a>
-          <a
-            href="https://soundcloud.com/oddboll"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nav-social-link"
-          >
-            <img
-              src={ScLogo}
-              alt="soundcloud logo"
-              className="nav-social-icon"
-            />
-          </a>
-          <a
-            href="https://www.mixcloud.com/kettle_whistle/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nav-social-link"
-          >
-            <img
-              src={MixcloudLogo}
-              alt="mixcloud logo"
-              className="nav-social-icon"
-            />
-          </a>
+            Interviews
+          </NavLink>
         </div>
-      </nav>
-    </div>
+
+        <div className="nav-link">
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+            to="/listen"
+          >
+            Listen
+          </NavLink>
+        </div>
+
+        <div className="nav-link">
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+            to="/3-word-reviews"
+          >
+            3-Word Reviews
+          </NavLink>
+        </div>
+
+        <div className="nav-link">
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+            to="/about"
+          >
+            About
+          </NavLink>
+        </div>
+      </div>
+      <div className="nav-social-logos">
+        <a
+          href="https://www.instagram.com/odddboll/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-social-link"
+        >
+          <img src={IgLogo} alt="instagram logo" className="nav-social-icon" />
+        </a>
+        <a
+          href="https://soundcloud.com/oddboll"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-social-link"
+        >
+          <img src={ScLogo} alt="soundcloud logo" className="nav-social-icon" />
+        </a>
+        <a
+          href="https://www.mixcloud.com/kettle_whistle/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-social-link"
+        >
+          <img
+            src={MixcloudLogo}
+            alt="mixcloud logo"
+            className="nav-social-icon"
+          />
+        </a>
+      </div>
+    </nav>
   );
 };
 
