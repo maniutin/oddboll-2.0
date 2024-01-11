ce31cd1daad398a5fc40c3659ac7603dc4f2affb
Nick <maniutin@gmail.com>
2024-01-11 14:48:05 -0500

    increase returned posts limit

--- src/hooks/useArticleData.tsx
+++ src/hooks/useArticleData.tsx
@@ -4,9 +4,13 @@ import axios from "axios";
 const useArticleData = () => {
   const [articleInfo, setArticleInfo] = useState({ articleData: [] });
 
+  const perPage = 100;
+
   useEffect(() => {
     axios
-      .get("http://localhost:8888/wordpress-trunk/wp-json/wp/v2/posts?_embed")
+      .get(
+        `http://localhost:8888/wordpress-trunk/wp-json/wp/v2/posts?_embed&per_page=${perPage}`
+      )
       .then((result) => {
         setArticleInfo((prev) => ({ ...prev, articleData: result.data }));
       })
